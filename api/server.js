eef94d923cb6e225953d4a3b851e68cb9c7b5033
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-12 21:54:33 -0400

    Initial setup

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+// Testing
+server.get("/", (req, res) => {
+	res.status(200).send("Hello there");
+});
+
+module.exports = server;
