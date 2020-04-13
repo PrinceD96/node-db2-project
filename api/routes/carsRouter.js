65e01300e54baa23c632f080219cfb6eaef94da4
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-13 02:15:59 -0400

    MVP + Stretch achieved! Wrote GET and POST requests

--- api/routes/carsRouter.js
+++ api/routes/carsRouter.js
@@ -12,4 +12,44 @@ router.get("/", (req, res) => {
 		);
 });
 
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+
+	db("cars")
+		.where({ id })
+		.first()
+		.then((car) =>
+			car
+				? res.status(200).json(car)
+				: res.status(404).json({ message: `Car with id ${id} not found` })
+		)
+		.catch((error) =>
+			res.status(500).json({ message: "Error retrieving car", error })
+		);
+});
+
+router.post("/", (req, res) => {
+	const car = req.body;
+
+	JSON.stringify(car) === "{}"
+		? res.status(400).json({ message: "missing car data" })
+		: !car.VIN || !car.make || !car.model || !car.mileage
+		? res.status(400).json({
+				message: `missing required ${
+					!car.VIN
+						? "VIN"
+						: !car.make
+						? "make"
+						: !car.model
+						? "model"
+						: !car.mileage
+						? "mileage"
+						: null
+				} field`
+		  })
+		: db("cars")
+				.insert(car)
+				.then((car) => res.status(201).json(car));
+});
+
 module.exports = router;
